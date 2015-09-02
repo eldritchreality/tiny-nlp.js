8d2e6d417d5756c567e3c44009a1a99192378b8f
eldritchreality <eldritchreality@gmail.com>
2015-09-02 17:22:39 +0000

    added some initial trivial functions and wiring

--- index.js
+++ index.js
@@ -1,2 +1,53 @@
-var salient = require("salient");
+var salient = require("salient"),
+    fs = require("fs"),
+    expect = require("chai").expect;
 
+var nlp = {}
+
+var stopwords = [];
+var nouns = [];
+var adjectives = [];
+var punctuation = ".?!,";
+var tweetTokenizer = new salient.tokenizers.TweetTokenizer();
+
+/* ****************************
+Load required data into memory
+   **************************** */
+void function loadStopWords() {
+    var file = fs.readFileSync(__dirname+"/data/stopwords.txt");
+    file = file.toString(); // was a buffer
+    stopwords = file.split("\n");
+} ()
+
+void function loadNouns() {
+    var file = fs.readFileSync(__dirname+"/data/nouns.txt");
+    file = file.toString(); // was a buffer
+    nouns = file.split("\n");
+} ()
+
+void function loadAdjectives() {
+     var file = fs.readFileSync(__dirname+"/data/adjectives.txt");
+     file = file.toString(); // was a buffer
+     adjectives = file.split("\n");
+} ()
+
+/* ****************
+Internal Functions
+   **************** */
+
+
+
+
+
+
+
+/* ************
+Module Exports
+   ************ */
+
+nlp.stopwords = stopwords;
+nlp.nouns = nouns;
+nlp.adjectives = adjectives;
+nlp.tweetTokenizer = tweetTokenizer;
+
+module.exports = nlp;
--- test/default_test.js
+++ test/default_test.js
@@ -1,43 +1,55 @@
 var should = require("chai").should();
-var nlp = require("index.js");
+var nlp = require(__dirname+"/../index.js");
 
-define("The NLP Library", function () {
+describe("The NLP Library", function () {
     
     it("Should load a list of Stopwords",function(done) {
-        
-        
-        
+        nlp.stopwords.should.be.an('array');
+        nlp.stopwords.should.have.length.of.at.least(500);
         done();
     });
     
-    it("Should load an array of known nouns",function() {});
+    it("Should load an array of known nouns",function(done) {
+        nlp.nouns.should.be.an('array');
+        nlp.nouns.should.have.length.of.at.least(500);
+        done();
+    });
     
-    it("Should load an array of known adjectives",function() {});
+    it("Should load an array of known adjectives",function(done) {
+        nlp.adjectives.should.be.an('array');
+        nlp.adjectives.should.have.length.of.at.least(500);
+        done();
+    });
     
-    it("Should load a part of speech tagging library",function() {});
-    
-    it("Should load an html entity decoder",function() {}); //should it really, sureley this is already built into node?
-    
-    it("Should normalise unicode punctuation",function() {});
+    it("Should normalise unicode punctuation",function() {throw new Error("unwritten test");});
     
     it("Should split text into sentences",function() {});
     
-    it("Should split sentences into word level tokens",function() {});
+    it("Should split sentences into word level tokens",function(done) {
+        var testSentence = "is this a proper sentence";
+        var expectedResult = ["is","this","a","proper","sentence"];
+        var result = nlp.tweetTokenizer.tokenize(testSentence);
+        
+        result.toString().should.equal(expectedResult.toString());
+        done()
+    });
     
-    it("Should be able to convert words into their stemmed forms",function() {}); // Get the 'stem' form of a word e.g. 'cats' -> 'cat'
+    it("Should be able to convert words into their stemmed forms",function() {throw new Error("unwritten test");}); // Get the 'stem' form of a word e.g. 'cats' -> 'cat'
     
-    it("Should be able to find interesting words in the corpus",function() {}); // not that important for my use case
+    it("Should be able to find interesting words in the corpus",function() {throw new Error("unwritten test");}); // not that important for my use case
     
-    it("Should be able to build a sentence out of a list of tikis",function() {});
+    it("Should be able to build a sentence out of a list of tikis",function() {throw new Error("unwritten test");});
     
-    it("Should be able to determine if we need a space between two tokens",function() {});
+    it("Should be able to determine if we need a space between two tokens",function() {throw new Error("unwritten test");});
     
-    it("Should be able to identify if a token is block of punctuation",function() {});
+    it("Should be able to identify if a token is block of punctuation",function() {throw new Error("unwritten test");});
     
-    it("Should be able to identify if a token is a stopword",function() {});
+    it("Should be able to identify if a token is a stopword",function() {throw new Error("unwritten test");});
     
-    it("Should be able to tell if output contains unmatched brackets or quotes",function() {});
+    it("Should be able to tell if output contains unmatched brackets or quotes",function() {throw new Error("unwritten test");});
     
-    it("Should be able to determine if a sequence is a subsequence of another sequence",function() {});
+    it("Should be able to determine if a sequence is a subsequence of another sequence",function() {throw new Error("unwritten test");});
     
-})
+    it("Should load an html entity decoder",function() {throw new Error("unwritten test");}); //should it really, sureley this is already built into node?
+    
+});
